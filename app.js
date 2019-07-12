430170dc5975c400e941c1530abad5df878ae506
cuscamayta <uscamayta.carlos@gmail.com>
2019-07-12 15:13:56 -0400

    fix jquery

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('./routes')(app);
 
 app.use(express.static(__dirname + '/public'));
 
-server.listen(process.env.PORT || 5000, () => {
+server.listen(process.env.PORT || 5000, () => {  
   console.log('listening on  port %d', server.address().port);
 });
 
