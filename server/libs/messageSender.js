bda5cf32ff3d40182213efcdad18cc26502260fd
cuscamayta <uscamayta.carlos@gmail.com>
2019-07-15 10:56:46 -0400

    remove code unnecesary

--- server/libs/messageSender.js
+++ server/libs/messageSender.js
@@ -16,14 +16,3 @@ exports.send = (message, number) => {
         })
         .then(message => logger.info(message.sid))
 }
-
-// const accountSid = 'AC64ed4254bc0dcf9753419d49893d6b88';
-// const authToken = 'e036d30e3d417ae2fe9faa27643b1314';
-// const client = require('twilio')(accountSid, authToken);
-
-// client.messages
-//   .create({
-//      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-//      from: '+19734594001',
-//      to: '+59160023359'
-//    })
